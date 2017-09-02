0801a69f91ab10811dc89d663d7cea8ce6643948
Victor Semionov <vsemionov@gmail.com>
2017-09-02 16:39:30 +0300

    create test objects inside tests to prevent missing table errors

--- example/api/tests.py
+++ example/api/tests.py
@@ -4,13 +4,12 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 
 
-user = User.objects.create(username='test', password='test')
-
-
 class TestUsers(APITestCase):
 
-    base_url = reverse('user-list')
-
     def test_list_success(self):
-        response = self.client.get(self.base_url + '?search=' + user.username)
+        user = User.objects.create(username='test', password='test')
+
+        base_url = reverse('user-list')
+
+        response = self.client.get(base_url + '?search=' + user.username)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
