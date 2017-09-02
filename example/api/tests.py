da4ce281477f1e3d7f77b3d1fb0c16460dcd7242
Victor Semionov <vsemionov@gmail.com>
2017-09-02 15:31:09 +0300

    basic list test

--- example/api/tests.py
+++ example/api/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+user = User.objects.create(username='test', password='test')
+
+
+class TestUsers(APITestCase):
+
+    base_url = reverse('user-list')
+
+    def test_list_success(self):
+        response = self.client.get(self.base_url + '?search=' + user.username)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
